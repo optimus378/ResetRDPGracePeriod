496c7f33d666bc54b4a970df6a6383cebff88c15
optimus378 <cmatthew.m@gmail.com>
2021-03-31 09:14:05 -0500

    Add files via upload

---
+++ ResetRDPGracePeriod.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+$definition = @"
+using System;
+using System.Runtime.InteropServices; 
+namespace Win32Api
+{
+	public class NtDll
+	{
+		[DllImport("ntdll.dll", EntryPoint="RtlAdjustPrivilege")]
+		public static extern int RtlAdjustPrivilege(ulong Privilege, bool Enable, bool CurrentThread, ref bool Enabled);
+	}
+}
+"@ 
+
+
+Add-Type -TypeDefinition $definition -PassThru
+$bEnabled = $false
+$res = [Win32Api.NtDll]::RtlAdjustPrivilege(9, $true, $false, [ref]$bEnabled)
+$key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod", [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree,[System.Security.AccessControl.RegistryRights]::takeownership)
+$acl = $key.GetAccessControl()
+$acl.SetOwner([System.Security.Principal.NTAccount]"Administrators")
+$key.SetAccessControl($acl)
+$rule = New-Object System.Security.AccessControl.RegistryAccessRule ("Administrators","FullControl","Allow")
+$acl.SetAccessRule($rule)
+$key.SetAccessControl($acl)
+Remove-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod'
+Start-Sleep -Seconds 5
+Remove-Variable * -ErrorAction SilentlyContinue
